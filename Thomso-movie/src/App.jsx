b5a28456d4cae7f4f46747fe3e75ee38a2f7b91f
Gourab <gourabkumargupta@gmail.com>
2024-04-09 13:26:46 +0530

    first commit

---
+++ Thomso-movie/src/App.jsx
@@ -0,0 +1,72 @@
+import './App.css';
+import { Cards } from './Components/Cards';
+
+
+function App() {
+  return <div className='App'>
+   <div className='A'>    <Cards  imgSrc="https://static.tvmaze.com/uploads/images/original_untouched/425/1064746.jpg"
+    imgAlt="Card image"
+    title="ALL AMERICAN"
+    description=" When a rising high school football player from South Central L.A. is recruited to play for Beverly Hills High, the wins, losses and struggles of two families from vastly different worlds — Compt..."
+    link="page"
+    buttonText="Know more"/>
+    
+
+    <Cards  imgSrc="https://static.tvmaze.com/uploads/images/original_untouched/413/1034988.jpg"
+    imgAlt="Card image"
+    title="ALL RISE"
+    description="All Rise is a courthouse drama that follows the chaotic, hopeful and sometimes absurd lives of its judges, prosecutors and public defenders, as they work with bailiffs, clerks and cops to get justice for the people of Los Angeles amidst a flawed legal process Among them is newly appointed Jud......"
+    link="cardPage"
+    buttonText="Know more"/>
+    <Cards  imgSrc="https://static.tvmaze.com/uploads/images/original_untouched/494/1235114.jpg"
+    imgAlt="Card image"
+    title="ALL THAT"
+    description="The new kids of comedy bring the funny as All That, America's #1 kid's sketch comedy show, returns with a new cast and a few familiar faces. From Executive Producers Kenan Thompson and Kel Mitchell"
+    link="cardPage"
+    buttonText="Know more"/>
+    </div>
+    <div className='A'> 
+    <Cards  imgSrc="https://static.tvmaze.com/uploads/images/original_untouched/494/1235111.jpg"
+    imgAlt="Card image"
+    title="ALL ACCESS"
+    description="All Access is an exclusive Showtime TV documentary series of 30-minute specials that chronicles the lives of athletes in unarmed combat sports and other tournamentas......."
+    link="cardPage"
+    buttonText="Know more"/>
+    <Cards  imgSrc="https://static.tvmaze.com/uploads/images/original_untouched/155/388118.jpg"
+    imgAlt="Card image"
+    title="ALL NIGHT"
+    description="After their graduation, the class of 2018 gathers to celebrate their last night together at their local rec center for Grad Night,an all-night party with dancing, karaoke, games—and ......"
+    link="cardPage"
+    buttonText="Know more"/>
+   <Cards  imgSrc="https://static.tvmaze.com/uploads/images/original_untouched/128/321026.jpg"
+    imgAlt="Card image"
+    title="ALL SOULS"
+    description="In a haunted Boston hospital called All Souls Hospital, Dr. Mitchell Grace's tries to find out the truth about the hauntings that dated as far back as the civil war. All Souls Hospital's lower levels......"
+    link="cardPage"
+    buttonText="Know more"/>
+    </div>
+    <div className='A'> 
+            <Cards  imgSrc="https://static.tvmaze.com/uploads/images/original_untouched/124/310674.jpg"
+    imgAlt="Card image"
+    title="ALL WRONG"
+    description="Carlos finds himself unemployed, broke, and in debt. When he finally manages to bring home a woman for a one-night stand, everything gets even worse........"
+    link="cardPage"
+    buttonText="Know more"/>
+            <Cards  imgSrc="https://static.tvmaze.com/uploads/images/original_untouched/408/1022051.jpg" width="130px"
+    imgAlt="Card image"
+    title="All Saints"
+    description="All Saints follows the lives of the staff at All Saints Western General Hospital. Until its closure in 2004, the show primarily focused on the staff ......"
+    link="cardPage"
+    buttonText="Know more"/>
+                <Cards  imgSrc="https://static.tvmaze.com/uploads/images/original_untouched/67/168214.jpg"
+    imgAlt="Card image"
+    title="ALL OUT"
+    description="In rugby, there is no ace striker, there is no number four batter, so who is the star of the team?The story begins at school entrance ceremony of Kanagawa High School where Kenji Gion, a small b...."
+    link="cardPage"
+    buttonText="Know more"/>
+    </div>
+
+  </div>;
+}
+
+export default App
